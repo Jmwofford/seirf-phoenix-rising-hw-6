4d2a1fbf4979e3f8ae6a0b76f8ccf22eceede548
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2019-12-16 19:56:27 -0700

    prime numbers complete

--- w02d03_homework_CharlieHumphrey/solutions.js
+++ w02d03_homework_CharlieHumphrey/solutions.js
@@ -4,6 +4,8 @@
 // Within a function, what is the difference between return and console.log?
 // Return will send the value of your function and stop the function. Console.log displayso on the console
 
+
+
 // Palindrome //
 const reverseString = (string) => {
     let lowCaps = string.toLowerCase(); // makes all letters in string lower case
@@ -17,6 +19,8 @@ console.log(reverseString("tacocat"));
 console.log(reverseString("Radar"));
 console.log(reverseString("Borscht"));
 
+
+
 // Sum Array //
 const sumArray = (num) => {
     let sum = 0 
@@ -26,5 +30,34 @@ const sumArray = (num) => {
 return sum
 }
 console.log(sumArray([1, 2, 3, 4, 5, 6]));
+
+
+
+// Prime Numbers //
+const checkPrime = (num) => {
+ // loop until the square root
+    for(i = 2; i <= Math.sqrt(num); i++) {
+        // if i equals 0 it is not prime
+        if (num % i === 0) {
+        return false
+    } 
+}
+//if the loops runs and never returns false, then it will jump to the next line & return true
+    return true
+ }
+console.log(checkPrime(5));
+
+// Prime - Part Two //
+const printPrimes = (prime) => {
+    //loop through all numbers until 'prime'
+    for(i = 0; i <= prime; i++) {
+    //if the number is prime it will be console.logged
+    if (checkPrime(i)) {
+       console.log(i)
+} 
+    }
+}
+//printPrimes(97);
+
 
 
