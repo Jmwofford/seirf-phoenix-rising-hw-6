a915111e4beae024e9c435c61595c6de01b25a8a
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2019-12-16 17:42:50 -0700

    verbal questions complete

---
+++ w02d03_homework_CharlieHumphrey/solutions.js
@@ -0,0 +1,6 @@
+// What is the difference between a parameter and an argument?
+// An argument is the input and the paramet is how the argument(input) is shown in the function
+
+// Within a function, what is the difference between return and console.log?
+// Return will send the value of your function and stop the function. Console.log displayso on the console
+
