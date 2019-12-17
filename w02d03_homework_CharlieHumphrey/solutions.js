af4d787733c7a9f18ba4a78cb642de98e3b96af3
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2019-12-16 18:34:21 -0700

    palindrome complete

--- w02d03_homework_CharlieHumphrey/solutions.js
+++ w02d03_homework_CharlieHumphrey/solutions.js
@@ -4,3 +4,17 @@
 // Within a function, what is the difference between return and console.log?
 // Return will send the value of your function and stop the function. Console.log displayso on the console
 
+const reverseString = (string) => {
+    let lowCaps = string.toLowerCase(); // makes all letters in string lower case
+    let splitString =lowCaps.split(''); // makes each letter in the string an array
+    let reverseArray = splitString.reverse(''); // reverses the array
+    let joinArray = reverseArray.join(''); // brings it all together in a string
+    return joinArray === string // compare the new array and string
+}
+
+console.log(reverseString("tacocat"));
+console.log(reverseString("Radar"));
+console.log(reverseString("Borscht"));
+
+
+
